b991e43825d0251814d315fe5b5537ce73382480
Victor Rivas <vctr90w@gmail.com>
2017-07-11 18:44:38 -0400

    Revert "Bootstrap template integration"

--- src/client/scripts.js
+++ src/client/scripts.js
@@ -3,18 +3,18 @@ var sourceDirectoryForCssDependencies = './styles/css/';
 var nodeModules = './../node_modules/';
 
 // CSS third party libraries
+//require(nodeModules + 'bootstrap/dist/css/bootstrap.min.css');
+
+//require(nodeModules + 'jquery/dist/jquery.min.js');
+//require(nodeModules + 'bootstrap/dist/css/bootstrap.min.css');
 require('bootstrap-loader');
-require('font-awesome/css/font-awesome.min.css');
-require('magnific-popup/dist/magnific-popup.css');
+require(sourceDirectoryForCssDependencies + 'styles.css');
 
-// Local css
-require(sourceDirectoryForCssDependencies + 'theme.css');
-
-// Local js dependencies
+// Example of how to bring css dependencies
+// require(sourceDirectoryForCssDependencies + 'test.css');
+// require(sourceDirectoryForCssDependencies + 'test.css');
+// Example of how to bring js dependencies
 require(sourceDirectoryForJsDependencies + 'mainAppLoader.js');
 require(sourceDirectoryForJsDependencies + 'modules/connection.js');
 require(sourceDirectoryForJsDependencies + 'modules/endpointBases.js');
 require(sourceDirectoryForJsDependencies + 'modules/Invitation.js');
-require(sourceDirectoryForJsDependencies + 'theme/theme.js');
-// This allows us to inject dependencies on the go as if we were writting requests directly on the js file
-//require('imports-loader?ScrollReveal=scrollreveal!./precompiled-dist/theme/theme.js');
--- webpack.config.js
+++ webpack.config.js
@@ -14,43 +14,21 @@ module.exports = {
   plugins: [
     new webpack.ProvidePlugin({
       $: "jquery",
-      jQuery: "jquery",
-      ScrollReveal: "ScrollReveal"
+      jQuery: "jquery"
     }),
     new WebpackBeforeBuildPlugin((compiler, continueCompilation) => {
       cleandir('./src/client/dist', error => console.log(error));
       cleandir('./src/client/styles/css', error => console.log(error));
       cleandir('./src/client/precompiled-dist', error => console.log(error));
       continueCompilation();
-    }),
+    })
   ],
   module: {
     loaders: [
-      { test: /\.css$/, loader: "style-loader!css-loader!resolve-url-loader" },
+      { test: /\.css$/, loader: "style-loader!css-loader" },
       {
-        test: /\.jpg$/, 
-        loader: "file-loader" 
-      },
-      { test: require.resolve('jquery'), loader: 'expose-loader?jQuery!expose-loader?$' },
-      {
-        test: /\.woff(2)?(¿v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "url-loader?limit=10000&mimetype=application/font-woff",
-        options: {
-          name: 'styles/fonts/[name].[ext]',
-            publicPath: function(url) {
-            return url.replace(/styles/, '..')
-          }
-        }
-      },
-      {
-        test: /\.(ttf|eot|svg)(¿v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "file-loader",
-        options: {
-          name: 'styles/fonts/[name].[ext]',
-            publicPath: function(url) {
-            return url.replace(/styles/, '..')
-          }
-        }
+        test: /\.woff($|¿)|\.woff2($|¿)|\.ttf($|¿)|\.eot($|¿)|\.svg($|¿)/,
+        loader: "url-loader"
       }
     ]
   }
