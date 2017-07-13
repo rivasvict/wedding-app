2eff824fa007eee459fb019af7b49af516047ce4
Victor Rivas <vctr90w@gmail.com>
2017-07-12 22:17:47 -0400

    Frontend model and module constructed

---
+++ src/client/app/modules/Comment.js
@@ -0,0 +1,58 @@
+(() => {
+  const CommentController = require('./../modules/Controller.js');
+
+  class Invitation {
+    constructor({ invitationId }) {
+      this.invitationId = invitationId;
+      this.comment = '';
+      this.controller = new CommentController({ invitationId });
+    }
+
+    fetchComment() {
+      return new Promise((resolve, reject) => {
+        this.controller.getCommentByInvitationId()
+          .then(response => {
+            this.comment = response.data.data;
+            resolve(response);
+          })
+          .catch(error => reject(error));
+      });
+    }
+
+    getComment() {
+      return this.comment;
+    }
+
+    setGuests(comment) {
+      this.comment = comment;
+    }
+
+    postComment(comment) {
+      return new Promise((resolve, reject) => {
+        this.controller.postComment(comment)
+          .then(response => {
+            this.synchrinizeCommentWithLocal(comment);
+            resolve(response);
+          })
+          .catch(error => reject(error));
+      });
+    }
+
+    synchrinizeCommentWithLocal(comment) {
+      this.setCommentToDefaultState();
+      this.updateCommentLocally(comment);
+    }
+
+    setCommentToDefaultState() {
+      this.comment = '';
+    }
+
+    updateCommentLocally(comment) {
+      this.guests = comment;
+    }
+
+  }
+
+  module.exports = Invitation;
+})();
+
