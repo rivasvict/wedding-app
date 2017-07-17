82aaee9fb8140f63dd5a4bd9920117ad6e36bae3
Victor <vctr90w@gmail.com>
2017-07-16 20:57:26 -0400

    Setup for deployment and test done

--- setup.sh
+++ setup.sh
@@ -3,6 +3,16 @@
 # This script is just meant to be used at the very first
 # setup of the project
 
+# Install ruby
+chmod +x configure-wedding-db.js
+sudo apt-add-repository ppa:brightbox/ruby-ng
+sudo apt-get update
+sudo apt-get install ruby2.2
+sudo ln -sf "$(which ruby2.2)" /usr/bin/ruby
+sudo apt-get install ruby2.2-dev
+sudo gem install rubygems-update
+sudo update_rubygems
+sudo gem update --system
 sudo apt-get -y install gem
 sudo gem install sass
 sudo npm install -g nvm
@@ -14,5 +24,6 @@ mkdir src/client
 mkdir src/client/app
 mkdir src/client/styles
 mkdir src/client/styles/sass
+mkdir src/client/styles/css
 mkdir src/client/precompiled-dist
 mkdir src/client/dist
